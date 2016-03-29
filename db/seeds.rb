68c92881ec1e05303ae6649957b80f90d6e56263
PKLong <patrickklong@gmail.com>
2016-03-29 15:26:45 -0400

    add some basic seed data for users and notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+patrick = User.create!(email: "patrick@aa.io", password: "password")
+gigi = User.create!(email: "gigi@aa.io", password: "password")
+
+Note.destroy_all
+
+p_note1 = patrick.notes.create!(title: "My First Note",
+                                body: "Feeling good about this note",
+                                notebook_id: 1)
+
+p_note2 = patrick.notes.create!(title: "My Second Note",
+                                body: "Buy milk",
+                                notebook_id: 1)
+
+
+g_note1 = gigi.notes.create!(title: "I love dogs",
+                                body: "I'm making a dog website",
+                                notebook_id: 2)
+
+g_note2 = gigi.notes.create!(title: "Getting married",
+                                body: "To the wonderful Max!",
+                                notebook_id: 2)
