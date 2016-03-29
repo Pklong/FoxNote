4ca1ba010a3b03c87af1ca32e4c87cf8a56c3758
PKLong <patrickklong@gmail.com>
2016-03-29 15:40:43 -0400

    add api/notes routes, npm packages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
   resource :session, only: [:new, :create, :destroy]
+
+  namespace :api, defaults: {format: :json} do
+    resources :notes, only: [:index, :update, :create, :show]
+  end
 end
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: path.join(__dirname, 'frontend', 'fox_note.jsx'),
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
