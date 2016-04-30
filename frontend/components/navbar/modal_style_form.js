5cbbe88b3e0baaa7a7f1dd38e6221913546b84ae
PKLong <patrickklong@gmail.com>
2016-04-30 13:03:06 -0400

    modify notebook form to cover full page

--- frontend/components/navbar/modal_style_form.js
+++ frontend/components/navbar/modal_style_form.js
@@ -5,12 +5,13 @@ module.exports = {
     left              : 0,
     right             : 0,
     bottom            : 0,
-    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    backgroundColor   : 'rgba(255, 255, 255, 0.75)',
+    zIndex            : '3'
   },
   content : {
     position                   : 'absolute',
     top                        : 0,
-    left                       : '89px',
+    left                       : 0,
     right                      : 0,
     bottom                     : 0,
     border                     : '1px solid #ccc',
@@ -19,6 +20,7 @@ module.exports = {
     WebkitOverflowScrolling    : 'touch',
     borderRadius               : 0,
     outline                    : 'none',
-    padding                    : '20px'
+    padding                    : '20px',
+    zIndex                     : '3'
   }
 };
